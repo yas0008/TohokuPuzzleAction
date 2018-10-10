d91685e0dbc9b684434d00ad2286b3ee1597e1ea
kai_f <kai_f>
2018-10-10 10:36:40 +0900

    no message

--- Assets/KiritanCannonSkill.cs
+++ Assets/KiritanCannonSkill.cs
@@ -6,11 +6,33 @@ public class KiritanCannonSkill : VoxeroidSkill
 {
     public override List<VoxeroidController> ExecuteSkill(VoxeroidController voxeroid)
     {
-        throw new System.NotImplementedException();
+        return FindNextVoxeroid(voxeroid);
     }
 
     protected override List<VoxeroidController> FindNextVoxeroid(VoxeroidController voxeroid)
     {
-        throw new System.NotImplementedException();
+        var list = new List<VoxeroidController>();
+
+        var positions = new List<Vector3>();
+        positions.Add(voxeroid.transform.position + voxeroid.GetForward() + Vector3.up + voxeroid.transform.right);
+        positions.Add(voxeroid.transform.position + voxeroid.GetForward() + Vector3.up + voxeroid.transform.right * -1);
+
+        positions.ForEach(p =>
+        {
+            Debug.Log(p);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(p, Vector3.down, out hit, 0.6f, LayerMaskUtility.Instance.GetLayerMask(LayerMaskUtility.LayerName.Player)))
+            {
+                VoxeroidController target = hit.collider.gameObject.transform.root.GetComponent<VoxeroidController>();
+                if (target != null)
+                {
+                    Debug.Log(target.name);
+                    list.Add(target);
+                }
+            }
+        });
+
+        return list;
     }
+
 }
